147b22c32dc239f1a7190c1ee0c624197fc9bf7c
Zach Cardais <zcardais@gmail.com>
2015-12-08 20:53:02 -0500

    Meets minimum specifications. I'll next add custom styling to make it my own.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,5 @@
-/********** BIO **********
-*************************/
+/* BIO
+----------------------------------- */
 var bio = {
 	"name":"Zach Cardais",
 	"role":"Data Scientist",
@@ -10,18 +10,63 @@ var bio = {
 		"twitter": "@zeffective",
 		"location": "Charlotte, NC"
 	},
-	"welcomeMessage": "Hi! This is a JavaScript-heavy site that I coded in November 2015. Thanks for visiting.",
+	"welcomeMessage": "Welcome to my online resume site. This is a JavaScript-heavy site that I coded in November and December of 2015. Thanks for visiting.",
 	"skills": [
 		"Google Analytics",
 		"Programming",
 		"Workshops and Training",
 		"Web Analytics"
 	],
-	"bioPic": "images/zjc.jpg"
+	"biopic": "images/zjc.jpg"
 }
 
-/********** WORK **********
-**************************/
+/* bio.display
+----------------------------------- */
+bio.display = function(){
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts, #footerContacts").append(formattedMobile);
+
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts, #footerContacts").append(formattedEmail);
+
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts, #footerContacts").append(formattedGithub);
+
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	$("#topContacts, #footerContacts").append(formattedTwitter);
+
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts, #footerContacts").append(formattedLocation);
+
+	var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+	$("#header").append(formattedBiopic);
+
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedWelcomeMsg);
+
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+		$("#skills").append(formattedSkill);
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+		$("#skills").append(formattedSkill);
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+		$("#skills").append(formattedSkill);
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+		$("#skills").append(formattedSkill);
+	}
+}
+$("#header").append(bio.display);
+
+
+/* WORK
+----------------------------------- */
 var work = {
 	"jobs":[
 		{
@@ -30,91 +75,20 @@ var work = {
 			"location": "Charlotte, NC",
 			"dates": "January 2015",
 			"description": "Web analytics and Google Analytics expert consultant and training."
+		},
+		{
+			"employer": "Sweet Relish",
+			"title": "VP Operations",
+			"location": "Huntersville, NC",
+			"dates": "April 2012 - July 2014",
+			"description": "Content Marketing platform for product companies. I was in charge of internal operations inlcluding online advertising and web analytics. I was responsible for creating dashboards and reporting to our team."
 		}
 	]
 }
 
-/********** PROJECTS **********
-******************************/
-var projects = {
-	"projects": [{
-		"title": "Online Resume",
-		"dates": "December 2015",
-		"description": "Online resume site using JavaScript, JSON and jQuery",
-		"images": ["images/online-resume.png", "images/portfolio.png"]
-	}, {
-		"title": "Portfolio site",
-		"dates": "November 2015",
-		"description": "Portfolio site to display my Udacity projects using HTML, CSS, JavaScript, JSON and jQuery",
-
-	}]
-}
-
-/********** EDUCATION **********
-*******************************/
-var education = {
-	"schools": [{
-		"name": "Virginia Tech",
-		"location": "Blacksburg, VA, US",
-		"degree": "BS",
-		"major": ["Finance", "Marketing"],
-		"dates": 2005,
-		"url": "http://www.vt.edu/"
-	}],
-	"onlineCourses": [{
-		"title": "Nanodegree Front-end Web Development",
-		"school": "Udacity",
-		"date": 2016,
-		"url": "http://www.udacity.com/nanodegree"
-	}]
-}
-
-/********** displayHeader **********
-*************************/
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-$("#topContacts, #footerContacts").append(formattedMobile);
-
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-$("#topContacts, #footerContacts").append(formattedEmail);
-
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-$("#topContacts, #footerContacts").append(formattedTwitter);
-
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-$("#topContacts, #footerContacts").append(formattedGithub);
-
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-$("#topContacts, #footerContacts").append(formattedLocation);
-
-var formattedBiopic = HTMLbioPic.replace("%data%", bio.bioPic);
-$("#header").append(formattedBiopic);
-
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$("#header").append(formattedWelcomeMsg);
-
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-	$("#skills").append(formattedSkill);
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-	$("#skills").append(formattedSkill);
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-	$("#skills").append(formattedSkill);
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-	$("#skills").append(formattedSkill);
-}
-
-
-/********** displayWork **********
-*************************/
-function displayWork() {
+/* work.display
+----------------------------------- */
+work.display = function() {
 	for (job in work.jobs) {
 		$("#workExperience").append(HTMLworkStart);
 
@@ -131,12 +105,27 @@ function displayWork() {
 		$(".work-entry:last").append(formattedDescription);
 	}
 }
+$("#workExperience").append(work.display);
 
-displayWork();
 
+/* PROJECTS
+----------------------------------- */
+var projects = {
+	"projects": [{
+		"title": "Online Resume",
+		"dates": "December 2015",
+		"description": "Online resume site using JavaScript, JSON and jQuery",
+		"images": ["images/resume.png", "images/resume2.png"]
+	}, {
+		"title": "Portfolio site",
+		"dates": "November 2015",
+		"description": "Portfolio site to display my Udacity projects using HTML, CSS, JavaScript, JSON and jQuery",
+		"images": ["images/portfolio.png", "images/portfolio2.png"]
+	}]
+}
 
-/********** displayProjects **********
-*************************************/
+/* projects.display
+----------------------------------- */
 projects.display = function() {
 	// display code goes here
 	for (project in projects.projects) {
@@ -148,8 +137,8 @@ projects.display = function() {
 		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
 		$(".project-entry:last").append(formattedDates);
 
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].images);
-		$(".project-entry:last").append(formattedImage);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
 		if (projects.projects[project].images.length > 0) {
 			for (image in projects.projects[project].images) {
 				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
@@ -163,8 +152,41 @@ projects.display = function() {
 $("#projects").append(projects.display);
 
 
-/********** displayEducation **********
-*******************************/
+/* EDUCATION
+----------------------------------- */
+var education = {
+	"schools": [{
+		"name": "Virginia Tech",
+		"location": "Blacksburg, VA",
+		"degree": "BS",
+		"major": ["Finance"],
+		"dates": 2005,
+		"url": "http://www.vt.edu/"
+	}, {
+		"name": "University of Lugano",
+		"location": "Lugano, Switzerland",
+		"degree": "BS",
+		"major": ["Marketing"],
+		"dates": "2005",
+		"url": "http://www.usi.ch/en"
+	}, {
+		"name": "Providence Day School",
+		"location": "Charlotte, NC",
+		"degree": "n/a",
+		"major": ["n/a"],
+		"dates": "1988 - 2001",
+		"url": "http://www.providenceday.org/"
+	}],
+	"onlineCourses": [{
+		"title": "Nanodegree Front-end Web Development",
+		"school": "Udacity",
+		"date": 2016,
+		"url": "http://www.udacity.com/nanodegree"
+	}]
+}
+
+/* education.display
+----------------------------------- */
 education.display = function() {
 	for (school in education.schools) {
 		$("#education").append(HTMLschoolStart);
@@ -172,14 +194,28 @@ education.display = function() {
 		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
 		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
 		var formattedNameDegree = formattedName + formattedDegree;
-		$(".education-entry").append(formattedNameDegree);
+		$(".education-entry:last").append(formattedNameDegree);
+
+		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		$(".education-entry:last").append(formattedDates);
+
+		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		$(".education-entry:last").append(formattedLocation);
+
+		var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+		$(".education-entry:last").append(formattedMajor);
 	}
 }
 $("#education").append(education.display);
 
 
-/********** internationalizeButon **********
-*******************************************/
+/* MAP
+----------------------------------- */
+$("#mapDiv").append(googleMap);
+
+
+/* internationalizeButton
+----------------------------------- */
 // function inName(name) {
 // 	name = name.trim().split(" ");
 // 	console.log(name);
@@ -194,10 +230,8 @@ $("#education").append(education.display);
 
 
 
-
-
-/********** NOTES FROM QUIZZES **********
-****************************************/
+/* NOTES FROM QUIZZES
+----------------------------------- */
 
 // $("#main").append("Zach Cardais");
 
